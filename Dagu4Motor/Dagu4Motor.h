6477c54bcf3fc817dc57f29d769db386b87f48f6
William <william@friedcircuits.us>
2013-11-24 18:37:05 -0800

    Cleaned up left over encoder code

--- Dagu4Motor/Dagu4Motor.h
+++ Dagu4Motor/Dagu4Motor.h
@@ -24,39 +24,27 @@ encB: Digital pin for encoder B
 #ifndef Dagu4Motor_h  
 #define Dagu4Motor_h
 //#define ENCODER_OPTIMIZE_INTERRUPTS //Fails to compile, vector errors
-#include "Encoder.h"
- 
 #include "Arduino.h" 
 
 class Dagu4Motor  
 {  
 public:  
-    Dagu4Motor(int pwmPin, int dirPin, int currPin, int encAPin, int encBPin); 
+    Dagu4Motor(int pwmPin, int dirPin, int currPin); 
 	void begin();
-    void stopMotors();  
-    void setSpeed(int speedMotor);  
-    void setMotorDirection(bool isMotor);  
+    	void stopMotors();  
+    	void setSpeed(int speedMotor);  
+    	void setMotorDirection(bool isMotor);  
 	int getCurrent();
-	float getDistance();
 	int getSpeed();
-	long int getTicks();
-	void resetTicks();
 
 	
 	
 private:  
     int _pwm;  
-    int _dir; 
-	int _curr;
-	int _currRate;
-	int _encA;
-	int _encB;
-	float _distance;
-	int _speed;
-	long int _ticks;
-	Encoder enc;
-
-	
+    int _dir;  
+    int _curr;
+    int _currRate;
+    int _speed;
 
 };
 
